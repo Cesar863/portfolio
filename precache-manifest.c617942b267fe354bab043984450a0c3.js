31cdce14bf68b643e564ff66326c1289f273ea9f
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2023-03-27 23:20:32 -0400

    Updates

---
+++ precache-manifest.c617942b267fe354bab043984450a0c3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "d51c1241b31188aa1e74",
+    "url": "/portfolio/static/css/main.22023799.chunk.css"
+  },
+  {
+    "revision": "d51c1241b31188aa1e74",
+    "url": "/portfolio/static/js/main.d51c1241.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/portfolio/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "5a705d59dd468a28214b",
+    "url": "/portfolio/static/css/2.8de1a76a.chunk.css"
+  },
+  {
+    "revision": "5a705d59dd468a28214b",
+    "url": "/portfolio/static/js/2.5a705d59.chunk.js"
+  },
+  {
+    "revision": "f800b1316a521fadcf37",
+    "url": "/portfolio/static/js/runtime~main.f800b131.js"
+  },
+  {
+    "revision": "295f8178403dd301dec69a77af1b6797",
+    "url": "/portfolio/static/media/Me.295f8178.png"
+  },
+  {
+    "revision": "efdcce6dead81c389f40eb53621fa36b",
+    "url": "/portfolio/static/media/Cesar Martinez Resume 2022-2023.efdcce6d.pdf"
+  },
+  {
+    "revision": "68a751757985d6511a4ddae0d7998706",
+    "url": "/portfolio/static/media/HTML-CSS-portfolio.68a75175.png"
+  },
+  {
+    "revision": "2606ee9622ce15115b1bbe150ee86e79",
+    "url": "/portfolio/static/media/PWA-budget-tracker.2606ee96.png"
+  },
+  {
+    "revision": "0318fc38fb193508f709d5b9f8e5a02c",
+    "url": "/portfolio/static/media/code-refactor.0318fc38.png"
+  },
+  {
+    "revision": "78a19f46ea6e7d3d3201132211f747fe",
+    "url": "/portfolio/static/media/eat-n-greet.78a19f46.png"
+  },
+  {
+    "revision": "178e2894c43f3c0603633ced77acd66c",
+    "url": "/portfolio/static/media/idk-you-pick.178e2894.png"
+  },
+  {
+    "revision": "29e0d895cae44927e50830c6b951cf4b",
+    "url": "/portfolio/static/media/javascript-quiz.29e0d895.png"
+  },
+  {
+    "revision": "92c4f4cd8bf89aa915ad955cfcce7cec",
+    "url": "/portfolio/static/media/password-generator.92c4f4cd.png"
+  },
+  {
+    "revision": "c9e36568a81930660ac035b0240e2fa4",
+    "url": "/portfolio/static/media/start-us.c9e36568.png"
+  },
+  {
+    "revision": "73b1f71c602cc90a26e22f146d1e7acf",
+    "url": "/portfolio/static/media/tech-blog.73b1f71c.png"
+  },
+  {
+    "revision": "ba027e3517047f8fd91c28eb46637cca",
+    "url": "/portfolio/static/media/work-day-scheduler.ba027e35.png"
+  },
+  {
+    "revision": "b165439e680d67a30171454c74e5d9e4",
+    "url": "/portfolio/index.html"
+  }
+];
