2c37bc9aa438cfed5cf992d926db96fe1a5ea4d6
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2023-04-08 22:12:53 -0400

    added cdn links

---
+++ build/precache-manifest.197394b463e7d50fd169e43783acb4bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eddf2d59ce4d8e376fa",
+    "url": "/portfolio/static/css/main.4e4e6e3e.chunk.css"
+  },
+  {
+    "revision": "8eddf2d59ce4d8e376fa",
+    "url": "/portfolio/static/js/main.8eddf2d5.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/portfolio/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "9e9de8f33111ffdc539d",
+    "url": "/portfolio/static/css/2.43906463.chunk.css"
+  },
+  {
+    "revision": "9e9de8f33111ffdc539d",
+    "url": "/portfolio/static/js/2.9e9de8f3.chunk.js"
+  },
+  {
+    "revision": "f8b95647bec3d732df71",
+    "url": "/portfolio/static/js/runtime~main.f8b95647.js"
+  },
+  {
+    "revision": "521cee3390a0707cb974ea3f9dab4aa9",
+    "url": "/portfolio/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3b788fb9c36e90f807162ff598801dc4.js"
+  "/portfolio/precache-manifest.197394b463e7d50fd169e43783acb4bd.js"
 );
 
 workbox.clientsClaim();
