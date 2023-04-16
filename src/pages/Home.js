ccbc8e7816ad5fd82fbf0595a1944f7bf86ac926
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2023-04-15 23:45:41 -0400

    dynamo db setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Content from '../components/Content';
 import About from '../components/About';
 import Portfolio from '../components/Portfolio';
 import Contact from '../components/Contact';
 import Resume from '../components/Resume';
+import { incrementViewCount } from '../utils/dynamodb';
 
 function Home({ currentPage }) {
+    const [hit, setHit] = useState(false);
+
+    if(!hit){
+        incrementViewCount();
+        setHit(true);
+    }
 
     const renderPage = () => {
         switch (currentPage.name) {
