7163c79785d03a571ea79849a2e92cf64acd8387
Cesar863 <cesarm.863@gmail.com>
2023-04-22 03:48:58 +0000

    Update build folder

---
+++ build/precache-manifest.bb8a09a370ecf937de22d56b43a2add2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7cf5ec43f9b20255d41",
+    "url": "/static/css/main.2503b6d2.chunk.css"
+  },
+  {
+    "revision": "a7cf5ec43f9b20255d41",
+    "url": "/static/js/main.f4b6a245.chunk.js"
+  },
+  {
+    "revision": "67f45efe784013c291c2",
+    "url": "/static/js/runtime~main.40425b10.js"
+  },
+  {
+    "revision": "4625d72796d48da763ad",
+    "url": "/static/css/2.4fd04ff4.chunk.css"
+  },
+  {
+    "revision": "4625d72796d48da763ad",
+    "url": "/static/js/2.367483cb.chunk.js"
+  },
+  {
+    "revision": "2a102ed781a96e0d5028",
+    "url": "/static/js/3.cabcfef1.chunk.js"
+  },
+  {
+    "revision": "358171efe46f5d87b108dbf2d769da2e",
+    "url": "/index.html"
+  }
+];
