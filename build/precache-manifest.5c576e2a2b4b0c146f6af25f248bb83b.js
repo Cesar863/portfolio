a800cc679ab7e3d7021604dca30b70b01ab79821
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2023-04-21 23:25:27 -0400

    wip3

---
+++ build/precache-manifest.5c576e2a2b4b0c146f6af25f248bb83b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c34609ce7463e501fd6",
+    "url": "/portfolio/static/css/main.d4b53a83.chunk.css"
+  },
+  {
+    "revision": "1c34609ce7463e501fd6",
+    "url": "/portfolio/static/js/main.1c34609c.chunk.js"
+  },
+  {
+    "revision": "48fc70a51432ec3cd299",
+    "url": "/portfolio/static/js/1.48fc70a5.chunk.js"
+  },
+  {
+    "revision": "ad29ee29621ca81e1fb2",
+    "url": "/portfolio/static/css/2.c8186b9d.chunk.css"
+  },
+  {
+    "revision": "ad29ee29621ca81e1fb2",
+    "url": "/portfolio/static/js/2.ad29ee29.chunk.js"
+  },
+  {
+    "revision": "40761c354ae8f0577f24",
+    "url": "/portfolio/static/js/runtime~main.40761c35.js"
+  },
+  {
+    "revision": "b52a7d03c5ae7646deda44d7c8ac0614",
+    "url": "/portfolio/index.html"
+  }
+];
