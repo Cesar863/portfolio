0adb1d56100c939dd6701a7959df7b3b8dc66339
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2023-04-21 22:33:55 -0400

    wip

--- src/assets/css/styledComponents.js
+++ src/assets/css/styledComponents.js
@@ -1,1 +1,41 @@
 import styled from "styled-components";
+
+export const NavContainer = styled.div `
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    color: #f7fff7;
+    padding: 5px 5px 0 5px;
+    `
+
+export const Header = styled.div `
+    font-size: 4em;
+    `
+
+export const Menu = styled.div `
+    display: flex;
+    align-items: center;
+    `
+
+export const MenuItem = styled.div `
+    padding: 0 10px 0 10px;
+    font-size: 1em;
+    cursor: pointer;
+    `
+export const MobileNavContainer = styled.div `
+    display: block;
+    text-align:center;
+    color: #f7fff7;
+    padding: 5px 5px 0 5px;
+    width: 100%;
+    `
+export const MobileNavHeader = styled.div `
+    font-size: 2em;
+    `
+export const MobileMenu = styled.div `
+    display: block;
+    `
+export const MobileMenuItem = styled.div `
+    display: block;
+    cursor: pointer;
+    `
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { capitalizeFirstLetter } from '../utils/helpers';
-import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { MobileMenu, MobileMenuItem, MobileNavContainer, MobileNavHeader, Menu, MenuItem, NavContainer, Header } from '../assets/css/styledComponents';
 
 function Nav(props) {
     const [screenWidth, setScreenWidth] = useState(window.innerWidth);
@@ -23,45 +23,6 @@ function Nav(props) {
     useEffect(() => {
         document.title = capitalizeFirstLetter(currentPage.name);
     }, [currentPage]);
-
-    const NavContainer = styled.div`
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
-    color: #f7fff7;
-    padding: 5px 5px 0 5px;
-    `
-
-    const Header = styled.div`
-    font-size: 4em;
-    `
-
-    const Menu = styled.div`
-    display: flex;
-    align-items: center;
-    `
-
-    const MenuItem = styled.div`
-    padding: 0 10px 0 10px;
-    font-size: 1em;
-    cursor: pointer;
-    `
-    const MobileNavContainer = styled.div`
-    display: block;
-    text-align:center;
-    color: #f7fff7;
-    padding: 5px 5px 0 5px;
-    width: 100%
-    `
-    const MobileNavHeader = styled.div`
-    font-size: 2em;
-    `
-    const MobileMenu = styled.div`
-    display: block;
-    `
-    const MobileMenuItem = styled.div`
-    display: block;
-    `
 
     return(
         <nav className='navbar navbar-expand-lg navbar-dark id="mainNav'>
